216e43efbc1b9b80bbc17ddd7b78dff4ca8ae470
fchafes <64428168+fchafes@users.noreply.github.com>
2024-06-04 19:18:23 -0300

    cambios del parallax, cambio ruta gif

--- src/components/home/HomeAbout/HomeAbout.js
+++ src/components/home/HomeAbout/HomeAbout.js
@@ -8,13 +8,13 @@ export const HomeAbout = () => {
     <>
       <section className="home-about">
         <Picture id="about-1" speed={-0.1} />
-        <Picture id="about-2" speed={0.3} />
+        <Picture id="about-2" speed={0.1} />
         <h3 className="about-title">SOBRE ATELIER</h3>
         <Picture id="about-3" speed={0} />
         <TextBox id="first-text-box" speed={-0.01} />
-        <Picture id="about-4" speed={0.2} />
+        <Picture id="about-4" speed={0.05} />
         <Picture id="about-5" speed={0} />
-        <TextBox id="second-text-box" speed={-0.1} />
+        <TextBox id="second-text-box" speed={-0.05} />
         <Picture id="about-6" speed={-0.05} />
         <Picture id="about-7" speed={0} />
       </section>
--- src/components/home/HomeAbout/picturesData.js
+++ src/components/home/HomeAbout/picturesData.js
@@ -1,39 +1,39 @@
 const picturesData = [
-    {
-      id: "about-1",
-      urlSmall: "/images/home/about_1_small.webp",
-      urlLarge: "/images/home/about_1_large.webp",
-    },
-    {
-      id: "about-2",
-      urlSmall: "/images/home/about_2_small.webp",
-      urlLarge: "/images/home/about_2_large.webp",
-    },
-    {
-      id: "about-3",
-      urlSmall: "/images/home/about_3_small.webp",
-      urlLarge: "/images/home/about_3_large.webp",
-    },
-    {
-      id: "about-4",
-      urlSmall: "/images/home/about_4_small.webp",
-      urlLarge: "/images/home/about_4_large.webp",
-    },
-    {
-      id: "about-5",
-      urlSmall: "/images/home/about_5_small.webp",
-      urlLarge: "/images/home/about_5_large.webp",
-    },
-    {
-      id: "about-6",
-      urlSmall: "/images/home/about_6_small.webp",
-      urlLarge: "/images/home/about_6_large.webp",
-    },
-    {
-      id: "about-7",
-      urlSmall: "/images/home/about_7_small.webp",
-      urlLarge: "/images/home/about_7_large.webp",
-    },
-  ];
+  {
+    id: "about-1",
+    urlSmall: "/images/home/about_1_small.webp",
+    urlLarge: "/images/home/about_1_large.webp",
+  },
+  {
+    id: "about-2",
+    urlSmall: "/images/home/about_2_small.webp",
+    urlLarge: "/images/home/about_2_large.webp",
+  },
+  {
+    id: "about-3",
+    urlSmall: "/images/home/home_gif_small.gif",
+    urlLarge: "/images/home/home_gif_large.gif",
+  },
+  {
+    id: "about-4",
+    urlSmall: "/images/home/about_4_small.webp",
+    urlLarge: "/images/home/about_4_large.webp",
+  },
+  {
+    id: "about-5",
+    urlSmall: "/images/home/about_5_small.webp",
+    urlLarge: "/images/home/about_5_large.webp",
+  },
+  {
+    id: "about-6",
+    urlSmall: "/images/home/about_6_small.webp",
+    urlLarge: "/images/home/about_6_large.webp",
+  },
+  {
+    id: "about-7",
+    urlSmall: "/images/home/about_7_small.webp",
+    urlLarge: "/images/home/about_7_large.webp",
+  },
+];
 
-  export default picturesData
+export default picturesData;
