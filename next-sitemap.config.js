865239509612ac55c350eabed6853bcf44d0a81c
fchafes <64428168+fchafes@users.noreply.github.com>
2024-07-10 16:08:33 -0300

    sitemap config update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   siteUrl: "https://atelier.com.uy",
-  generateRobotsTxt: true, // (Opcional) Si quieres generar un archivo robots.txt
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  transform: async (config, url) => {
+    if (url === "/") {
+      return {
+        loc: url,
+        changefreq: "daily",
+        priority: 1.0,
+        lastmod: new Date().toISOString(),
+      };
+    }
+    // Configuración por defecto para las demás URLs
+    return {
+      loc: url,
+      changefreq: "daily",
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
